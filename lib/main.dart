52a6a1bd044d43ed07b4d60343bb9286c7203848
Kamil Klukowski <k.klukowski@nomtek.com>
2018-05-11 16:21:14 +0200

    vs code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   MyApp({Key key}) : super(key: key);
 
   @override
@@ -17,58 +16,53 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: title,
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(title),
-        ),
-        body: new ListView.builder(
-          itemBuilder: (context, index) {
-		return new Column(
-			children: <Widget>[
-		            new Row(
-	 			children: <Widget>[
-					new FadeInImage.memoryNetwork(
-				        	placeholder: kTransparentImage,
-                				image:
-				                    'https://picsum.photos/64/64/?image=$index',
-		              		),
-					new Text(' $index'),
-				],
-	    		   ),
-			   new Divider(height: 1.0, indent: 64.0),
-			],
-		);
-          },
-        ),
-	floatingActionButton: new MyButton()
-      ),
+          appBar: new AppBar(
+            title: new Text(title),
+          ),
+          body: new ListView.builder(
+            itemBuilder: (context, index) {
+              return new Column(
+                children: <Widget>[
+                  new Row(
+                    children: <Widget>[
+                      new CachedNetworkImage(
+                        placeholder: new CircularProgressIndicator(),
+                        imageUrl: 'https://picsum.photos/64/64/?image=$index',
+                        errorWidget: new Icon(Icons.error),
+                      ),
+                      new Text(' $index'),
+                    ],
+                  ),
+                  new Divider(height: 1.0, indent: 64.0),
+                ],
+              );
+            },
+          ),
+          floatingActionButton: new MyButton()),
     );
   }
 }
 
-
 class MyButton extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-	return new FloatingActionButton(
-          child: new Icon(Icons.add),
-          onPressed: () {
-		final date = new DateTime.now();
-		showDatePicker(
-                    context: context,
-                    initialDate: date,
-                    firstDate: date.subtract(const Duration(days: 30)),
-                    lastDate: date.add(const Duration(days: 30))
-                  )
-                  .then((DateTime value) {
-			var alert = new AlertDialog(
-			      title: new Text("Selected date"),
-			      content: new Text("${value.year}-${value.month}-${value.day}"),
-			    );
-			showDialog(context: context, child: alert);
-                  });
-          }
-        );
+    return new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: () {
+          final date = new DateTime.now();
+          showDatePicker(
+                  context: context,
+                  initialDate: date,
+                  firstDate: date.subtract(const Duration(days: 30)),
+                  lastDate: date.add(const Duration(days: 30)))
+              .then((DateTime value) {
+            var alert = new AlertDialog(
+              title: new Text("Selected date"),
+              content: new Text("${value.year}-${value.month}-${value.day}"),
+            );
+            showDialog(
+                context: context, builder: (BuildContext context) => alert);
+          });
+        });
   }
-
 }
